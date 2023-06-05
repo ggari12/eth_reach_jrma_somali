052e8b5ed239dbaa1641f33d712259b448b82892
ACTED\Getu Gari <getu.boja22@yahoo.com>
2023-06-05 14:59:38 +0300

    checks for data collection

---
+++ R/checks_for_data_collection.R
@@ -0,0 +1,97 @@
+# checks for data collection
+library(tidyverse)
+library(lubridate)
+library(glue)
+library(supporteR)
+
+# read data and tool ----------------------------------------------------------
+# data
+df_tool_data <- readxl::read_excel("inputs/ETH2303_jrma_somali_data.xlsx") |>  
+    mutate(start = as_datetime(start),
+           end = as_datetime(end)) |> 
+    checks_add_extra_cols(input_enumerator_id_col = "enumerator_id",
+                          input_location_col = "hh_kebele")
+
+# tool
+loc_tool <- "inputs/ETH2303_jrma_somali_data.xlsx"
+
+df_survey <- readxl::read_excel(loc_tool, sheet = "survey")
+df_choices <- readxl::read_excel(loc_tool, sheet = "choices")
+
+# checks ------------------------------------------------------------------
+
+checks_output <- list()
+
+# testing data ------------------------------------------------------------
+
+df_testing_data <- df_tool_data |> 
+    filter(i.check.start_date < as_date("2023-06-05")) |> 
+    mutate(i.check.type = "remove_survey",
+           i.check.name = "",
+           i.check.current_value = "",
+           i.check.value = "",
+           i.check.issue_id = "logic_c_testing_data",
+           i.check.issue = "testing_data",
+           i.check.other_text = "",
+           i.check.checked_by = "",
+           i.check.checked_date = as_date(today()),
+           i.check.comment = "", 
+           i.check.reviewed = "1",
+           i.check.adjust_log = "",
+           i.check.so_sm_choices = "") |> 
+    dplyr::select(starts_with("i.check.")) |> 
+    rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_testing_data")
+
+# Time checks -------------------------------------------------------------
+
+# Time interval for the survey
+min_time_of_survey <- 30
+max_time_of_survey <- 120
+
+df_c_survey_time <-  supporteR::check_survey_time(input_tool_data = df_tool_data, 
+                                                  input_enumerator_id_col = "enumerator_id",
+                                                  input_location_col = "hh_kebele",
+                                                  input_min_time = min_time_of_survey, 
+                                                  input_max_time = max_time_of_survey)
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_c_survey_time")
+
+# check duplicate uuids ---------------------------------------------------
+
+df_c_duplicate_uuid <-  supporteR::checks_duplicate_uuids(input_tool_data = df_tool_data)
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_c_duplicate_uuid")
+
+# outliers ----------------------------------------------------------------
+
+df_c_outliers <- supporteR::check_outliers_cleaninginspector(input_tool_data = df_tool_data,
+                                                             input_enumerator_id_col = "enumerator_id",
+                                                             input_location_col = "hh_kebele")
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_c_outliers")
+
+# other_specify -----------------------------------------------------------
+
+df_others_data <- supporteR::extract_other_specify_data(input_tool_data = df_tool_data, 
+                                                        input_enumerator_id_col = "enumerator_id",
+                                                        input_location_col = "hh_kebele",
+                                                        input_survey = df_survey,  
+                                                        input_choices = df_choices)
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_others_data")
+
+
+# logical checks ----------------------------------------------------------
+
+
+
+
+# combined  checks --------------------------------------------------------
+
+df_combined_checks <- bind_rows(checks_output)
+
+# output the log
+write_csv(x = df_combined_checks, file = paste0("outputs/", butteR::date_file_prefix(), "_combined_checks_eth_msna_oromia.csv"), na = "")
+
